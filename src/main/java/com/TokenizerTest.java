1d66518033cf16ce60deaf3eda97b73910c4057d
Paul Morano <paul.morano@uconn.edu>
2018-03-21 09:02:02 -0400

    Simple tokenizer test

---
+++ src/main/java/com/TokenizerTest.java
@@ -0,0 +1,18 @@
+package com;
+
+import java.util.StringTokenizer;
+
+public class TokenizerTest {
+
+	public static void main(String[] args) {
+		String message = " Some code - some description";
+
+		StringTokenizer parser = new StringTokenizer(message,"-");
+		
+		if (parser.hasMoreTokens()) {
+			String firsttoken = parser.nextToken();
+			System.out.println("First token: [" + firsttoken + "]");
+			System.out.println("First token trim: [" + firsttoken.trim() + "]");
+		}
+	}
+}
